6d309dba06fa76ec24590bc219c542860bdde483
Oludowole Olumide <xeonncodes@gmail.com>
2024-10-04 09:22:56 +0100

    Added login and registration logic

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -4,10 +4,10 @@ import type { NextApiRequest, NextApiResponse } from "next";
 type Data = {
   name: string;
 };
-
+//
 export default function Handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data>
 ) {
   res.status(200).json({ name: "John Doe" });
 }
